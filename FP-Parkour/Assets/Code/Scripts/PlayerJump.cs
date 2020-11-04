433fff2e9644829946b1c35ae505e8daaed7d365
Frank van Kan <Frank.vanKan@student.hu.nl>
2020-11-04 15:13:37 +0100

    Implemented simple movement
    Simple movement with running, jumping and crouching

---
+++ FP-Parkour/Assets/Code/Scripts/PlayerJump.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Jumping & Gravity")]
+    [SerializeField]
+    private float jumpHeight = 2f;
+    [SerializeField]
+    private float gravity = -9.81f;
+    [SerializeField]
+    private float groundDistance = 0.1f;
+    [SerializeField]
+    private LayerMask groundMask = new LayerMask();
+    private Transform groundCheck;
+
+    [HideInInspector]
+    public bool isGrounded;
+    private Vector3 velocity;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        groundCheck = transform.Find("Ground Check");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ FP-Parkour/Assets/Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Speed")]
+
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float sprintModifer = 1.5f;
+    [HideInInspector]
+    public bool sprinting;
+    public float Speed
+    {
+        get
+        {
+            float realSpeed = speed;
+            realSpeed *= sprinting ? sprintModifer : 1;
+            realSpeed *= crouchScript.crouching ? crouchScript.crouchSpeedModifier : 1;
+            return realSpeed;
+        }
+    }
+
+    private CharacterController controller;
+    private PlayerCrouch crouchScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        crouchScript = GetComponent<PlayerCrouch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
+        sprinting = Input.GetAxisRaw("Sprint") != 0;
+
+        Vector3 movement = transform.right * xMovement + transform.forward * zMovement;
+
+        controller.Move(movement * Speed * Time.deltaTime);
+    }
+}
