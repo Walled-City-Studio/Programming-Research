28aad92d3372b5c893f881d62d4f2196a2928feb
DenisED1 <denis.edelbroek@student.hu.nl>
2020-11-11 13:47:29 +0100

    Main Menu Demo

---
+++ MainMenu/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //This line is to make the quit work in the editor
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
