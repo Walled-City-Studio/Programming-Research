01fd2bf1a7398d21e933b7b8f088ed998bad0a37
DenisED1 <denis.edelbroek@student.hu.nl>
2020-11-13 14:07:50 +0100

    Working Play Button
    Loads another scene

--- GUI-main-menu/Assets/Scripts/ButtonTests.cs
+++ GUI-main-menu/Assets/Scripts/ButtonTests.cs
@@ -1,13 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Reflection;
+using Unity.Collections;
+using Unity.UIElements.Runtime;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class ButtonTests : MonoBehaviour
 {
-    private VisualElement rootElement;
-    private void OnEnable()
+    //UI Elements
+    private PanelRenderer panelRenderer;
+
+    private UQueryBuilder<VisualElement> itemUIQuery;
+
+    private UQueryBuilder<Button> but;
+
+    private UQueryBuilder<Button> but2;
+
+    private void Start()
     {
-        rootElement = new VisualElement();
+        panelRenderer = GetComponent<PanelRenderer>();
+        itemUIQuery = panelRenderer.visualTree.Query<VisualElement>(classes: "screen");
+        //but = panelRenderer.visualTree.Query<Button>(classes: "menu-button");
+
+        Button but2 = panelRenderer.visualTree.Query<Button>("playButton").First();
+
+        but2.clickable.clicked += delegate { test(1); };
+
+        // but.ForEach(asd => {
+        //     asd.clickable.clicked += delegate { Debug.Log("huiyioogr"); };
+        // });
+    }
+
+    void Update()
+    {
+
+    }
+
+    void test(int i)
+    {
+        Debug.Log(i);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
