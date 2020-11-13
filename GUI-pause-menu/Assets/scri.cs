1375606c7337b576c7071cf655821bc90ee4b34e
Thomas Beumer <thomasbeumer@live.nl>
2020-11-13 11:23:35 +0100

    Add last with event

--- GUI-pause-menu/Assets/scri.cs
+++ GUI-pause-menu/Assets/scri.cs
@@ -1,7 +1,9 @@
 ﻿
+using System.Reflection;
 using Unity.Collections;
 using Unity.UIElements.Runtime;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class scri : MonoBehaviour
@@ -9,73 +11,51 @@ public class scri : MonoBehaviour
 
     //UI Elements
     private PanelRenderer panelRenderer;
+
     private UQueryBuilder<VisualElement> itemUIQuery;
 
     private UQueryBuilder<Button> but;
 
-    void Update()
+    private UQueryBuilder<Button> but2;
+
+    private void Start()
     {
-        //Get Entities And Data
-        //var playerDirtyEntities = playerDirtyEntityQuery.ToEntityArray(Allocator.TempJob);
-        //var itemEntities = itemEntityQuery.ToEntityArray(Allocator.TempJob);
-        //var itemOwners = itemEntityQuery.ToComponentDataArray<ActorInventoryItemOwnerComponent>(Allocator.TempJob);
-
-        //If Inventory Updated Do Logic
-        //if (playerDirtyEntities.Length != 0)
-        //{
-        Debug.Log("asdasd");
-        //Get Panel Renderer | Get All VisualElements with "Inventory-Item" Class
         panelRenderer = GetComponent<PanelRenderer>();
-            itemUIQuery = panelRenderer.visualTree.Query<VisualElement>(classes: "screen");
+        itemUIQuery = panelRenderer.visualTree.Query<VisualElement>(classes: "screen");
+        but = panelRenderer.visualTree.Query<Button>(classes: "menu-button");
 
+        but2 = panelRenderer.visualTree.Query<Button>(name: "I-button");
 
-        but = panelRenderer.visualTree.Query<Button>(classes: "I-button");
+        but2.ForEach(aa => {
+            but2.text = "asdaaaa11111";
+        });
 
         but.ForEach(asd => {
-            Debug.Log("123");
+ 
             asd.text = "adsasd";
+            //asd.RegisterCallback<KeyDownEvent>((evt) => Debug.Log(evt.keyCode));
+            asd.clickable.clicked += delegate { test(1); };
+
+           /* Clickable clickable = asd.clickable;
+            MouseDownEvent mouseDownEvent = MouseDownEvent.GetPooled();
+            //clickable.Invoke();
+            MethodInfo dynMethod = clickable.GetType().GetMethod("Invoke",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+            dynMethod.Invoke(clickable, new object[] { mouseDownEvent });
+            mouseDownEvent.Dispose();*/
+            //asd.onClick.AddListener(() => test(button1));
+
         });
-            //Loop Through All Ivnentory Items VisualElements
-            itemUIQuery.ForEach(inventoryItemVisualElement =>
-            {
-                Debug.Log("asdasd");
-                //inventoryItemVisualElement.style.top = 20;
-                //if (itemUIQueryIndex < itemEntities.Length)
-                //{
-                //Get InventoryItemUISHaredComponent
-                /*       var actorInventoryItemUISharedComponent = EntityManager.GetSharedComponentData<ActorInventoryItemUISharedComponent>(itemEntities[itemUIQueryIndex]);
-
-                       //Create Icon
-                       var iconVisualElement = new VisualElement();
-                       iconVisualElement.AddToClassList("Inventory-Item-Icon");
-                       iconVisualElement.style.backgroundImage = actorInventoryItemUISharedComponent.icon;
-                       inventoryItemVisualElement.Add(iconVisualElement);
-
-                       //Create Amount
-                       var amountVisualElement = new Label();
-                       amountVisualElement.AddToClassList("Inventory-Item-Amount");
-                       amountVisualElement.text = "1";
-                       inventoryItemVisualElement.Add(amountVisualElement);*/
-                //}
-                //else
-                //{
-                //Get Rid Of Icon
-           /*     if (inventoryItemVisualElement.childCount > 0)
-                        inventoryItemVisualElement.RemoveAt(0);
-
-                    //Get Rid Of Amount
-                    if (inventoryItemVisualElement.childCount > 0)
-                        inventoryItemVisualElement.RemoveAt(0);*/
-               //}
-
-                //itemUIQueryIndex++;
-            });
-        //}
-
-        //Dispose
-        //playerDirtyEntities.Dispose();
-        //itemEntities.Dispose();
-        //itemOwners.Dispose();
     }
+    void Update()
+    {
+      
+    }
+
+    void test(int i)
+    {
+        Debug.Log(i);
+    }
+
 }
 
