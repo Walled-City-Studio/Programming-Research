cf2ca011547edf04635195f075cb3c80284655d0
DenisED1 <denis.edelbroek@student.hu.nl>
2020-11-13 13:19:28 +0100

    Button om events te testen
    Niet veel bijzonders, moet ff commiten om wat van een andere branch te pullen.

---
+++ GUI-main-menu/Assets/Scripts/ButtonTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonTests : MonoBehaviour
+{
+    private VisualElement rootElement;
+    private void OnEnable()
+    {
+        rootElement = new VisualElement();
+    }
+}
