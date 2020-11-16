3f33a0143d0d787db29673e59af6252b26da27da
Frank van Kan <Frank.vanKan@student.hu.nl>
2020-11-16 16:44:20 +0100

    Refactored according to code conventions

--- Dynamic-Lighting/Assets/Code/Scripts/Player/MouseLook.cs
+++ Dynamic-Lighting/Assets/Code/Scripts/Player/MouseLook.cs
@@ -2,34 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+namespace Code.Scripts.Player
 {
-    [SerializeField]
-    private float mouseSensitivity = 100f;
-    [SerializeField]
-    private float aspectRatio = 16 / 9;
-
-    private Transform playerBody;
-    private float xRotation = 0f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class MouseLook : MonoBehaviour
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        playerBody = transform.parent;
+        [SerializeField]
+        private float mouseSensitivity = 100f;
+        [SerializeField]
+        private float aspectRatio = 16 / 9;
+
+        private Transform playerBody;
+        private float xRotation = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            playerBody = transform.parent;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            xRotation -= mouseY * mouseSensitivity * Time.deltaTime;
+            xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX * mouseSensitivity * aspectRatio * Time.deltaTime);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        xRotation -= mouseY * mouseSensitivity * Time.deltaTime;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX * mouseSensitivity * aspectRatio * Time.deltaTime);
-
-    }
 }
--- Dynamic-Lighting/Assets/Code/Scripts/Player/PlayerInput.cs
+++ Dynamic-Lighting/Assets/Code/Scripts/Player/PlayerInput.cs
@@ -2,41 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+namespace Code.Scripts.Player
 {
-    public Vector2 MovementInput
+    public class PlayerInput : MonoBehaviour
     {
-        get
+        public Vector2 MovementInput
         {
-            float sidewaysMovement = Input.GetAxisRaw("Horizontal");
-            float forwardMovement = Input.GetAxisRaw("Vertical");
-            Vector2 movement = new Vector2(sidewaysMovement, forwardMovement);
-            return Vector3.ClampMagnitude(movement, 1);
+            get
+            {
+                float sidewaysMovement = Input.GetAxisRaw("Horizontal");
+                float forwardMovement = Input.GetAxisRaw("Vertical");
+                Vector2 movement = new Vector2(sidewaysMovement, forwardMovement);
+                return Vector3.ClampMagnitude(movement, 1);
+            }
+
         }
 
+        public bool Sprint
+        {
+            get
+            {
+                return Input.GetButtonDown("Sprint");
+            }
+        }
+        public bool Sprinting
+        {
+            get
+            {
+                return Input.GetButton("Sprint");
+            }
+        }
+
+        public bool Crouch
+        {
+            get
+            {
+                return Input.GetButtonDown("Crouch");
+            }
+        }
+
+        public bool Crouching
+        {
+            get
+            {
+                return Input.GetButton("Crouch");
+            }
+        }
+
+        public bool Jump
+        {
+            get
+            {
+                return Input.GetButtonDown("Jump");
+            }
+        }
     }
 
-    public bool Sprint
-    {
-        get { return Input.GetButtonDown("Sprint"); }
-    }
-    public bool Sprinting
-    {
-        get { return Input.GetButton("Sprint"); }
-    }
-
-    public bool Crouch
-    {
-        get { return Input.GetButtonDown("Crouch"); }
-    }
-
-    public bool Crouching
-    {
-        get { return Input.GetButton("Crouch"); }
-    }
-
-    public bool Jump
-    {
-        get { return Input.GetButtonDown("Jump"); }
-    }
 }
+
