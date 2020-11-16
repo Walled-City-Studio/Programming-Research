819e04fc845bd460a824cfed794d9df9bcb06ce7
Frank van Kan <Frank.vanKan@student.hu.nl>
2020-11-16 16:29:16 +0100

    Finished lighting experiments
    Finished experimenting with different forms of lighting and integrated
    the day/night cycle into the realtime lighting scene

---
+++ Dynamic-Lighting/Assets/Code/Scripts/ScriptableObjects/DayNightSettings.cs
@@ -0,0 +1,13 @@
+﻿using Code.Scripts.Environment.TimeSystem;
+using UnityEngine;
+
+namespace Code.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "DayNightPreset", menuName = "Time/DayNightPreset", order = 1)]
+    public class DayNightSettings : ScriptableObject
+    {
+        // Sunrise, max sun intensity, sunset, dark
+        public GameDateTime dawn, noon, dusk, night;
+        public Gradient ambientColor;
+    }
+}
